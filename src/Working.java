df5e003ec0fbe28434d0d54ec4f3fecafa7d24d2
Ramit <ramit19086@iiitd.ac.in>
2020-11-16 23:26:17 +0530

    trying

--- src/Working.java
+++ src/Working.java
@@ -1,7 +1,6 @@
 public class Working {
     public static void main(String[] args) {
-        System.out.println("5464854834");
-        System.out.println();
+        System.out.println("hello");
 
     }
 }
